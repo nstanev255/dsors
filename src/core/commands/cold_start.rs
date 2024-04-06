acf80835b822e6d242382904f21898cd3116f214
nstanev255 <nikolay.stanev.255@gmail.com>
2024-04-06 23:46:04 +0300

    Added cold_start module

--- src/core/commands/cold_start.rs
+++ src/core/commands/cold_start.rs
@@ -7,10 +7,10 @@ use crate::core::events::handlers::hello::{HelloEvent, Identify};
 use crate::core::websocket::{Credentials};
 
 /**
-  The idea for this method is to catch the initial response from the discord api, which is actually the heartbeat interval...
-*/
+The idea for this method is to catch the initial response from the discord api, which is actually the heartbeat interval...
+ */
 pub async fn get_heartbeat_interval(socket: &mut SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>) -> u64 {
-    let message =  socket.next().await.unwrap().expect("error reading from socket");
+    let message = socket.next().await.unwrap().expect("error reading from socket");
     let hello_event: HelloEvent = serde_json::from_str(message.to_text().unwrap()).unwrap();
 
     println!("{:?}", hello_event);
@@ -22,7 +22,7 @@ pub fn create_identify(credentials: &Credentials) -> Identify {
     // After we get the HelloEvent, we should send back an identity message, with the proper credentials...
     let token = &credentials.token;
 
-    let identify_intent = Identify {
+    Identify {
         op: Opcode::Identify,
         d: crate::core::events::handlers::hello::IdentifyData {
             token: token.to_string(),
@@ -31,10 +31,8 @@ pub fn create_identify(credentials: &Credentials) -> Identify {
             properties: crate::core::events::handlers::hello::IdentifyProperties {
                 os: String::from("linux"),
                 device: String::from("dsors"),
-                browser: String::from("dsors")
-            }
-        }
-    };
-
-    identify_intent
+                browser: String::from("dsors"),
+            },
+        },
+    }
 }
